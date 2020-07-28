c3a3db00173cc0362d37e334b454ac791e6388ae
Parker Johnson <plj06370@creighton.edu>
2020-07-28 11:53:06 -0500

    Understanding JSX

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import ReactDOM from 'react-dom'
 const tasks = ['Take out the trash','Take out the trash', 'Walk the dogs', 'Buy groceries']
 
 const element = 
-    <ol>
-      <li>Hello world</li>
-      <li>Hello world</li>
-      <li>Hello world</li>
-    </ol>
+    <div>
+      <h1>
+        Task list!
+      </h1>
+      <ol>
+        { tasks.map((task, index) => <li key = {index}>{task}</li>) }
+      </ol>
+    </div>
 ReactDOM.render(element, document.getElementById('root'))
