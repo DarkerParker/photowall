a662ed4a181aec3feacf2ceca545c47219046cc7
Parker Johnson <plj06370@creighton.edu>
2020-07-28 11:37:13 -0500

    virtual DOM, and adding elements using React.createElement

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const tasks = ['Take out the trash','Take out the trash', 'Walk the dogs', 'Buy groceries']
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const element = 
+    <ol>
+      <li>Hello world</li>
+      <li>Hello world</li>
+      <li>Hello world</li>
+    </ol>
+ReactDOM.render(element, document.getElementById('root'))
