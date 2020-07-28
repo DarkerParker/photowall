9fe8bc88cbce00ab8aa0319bf592be1f85ebfc32
Parker Johnson <plj06370@creighton.edu>
2020-07-28 12:11:51 -0500

    fundamentals of the compositional model

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 
 const tasks = ['Take out the trash','Take out the trash', 'Walk the dogs', 'Buy groceries']
 
-const element = 
-    <div>
-      <h1>
-        Task list!
-      </h1>
-      <ol>
-        { tasks.map((task, index) => <li key = {index}>{task}</li>) }
-      </ol>
-    </div>
-ReactDOM.render(element, document.getElementById('root'))
+class List extends Component{
+
+  render(){
+    return (<ol>
+              { tasks.map((task, index) => <li key = {index}>{task}</li>) }
+            </ol>)
+  }
+
+}
+
+class Title extends Component{
+  render(){
+    return (<h1>Task list!</h1>)
+  }
+}
+
+class Main extends Component{
+  render(){
+    return (<div>
+      <Title/>
+      <List/>
+      <List/>
+      <List/>
+    </div>)
+  }
+}
+
+ReactDOM.render(<Main/>, document.getElementById('root'))
