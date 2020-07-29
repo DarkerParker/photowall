a0b8a4ce26fafd79a174fea0c09efb13b72efdf8
Parker Johnson <plj06370@creighton.edu>
2020-07-29 13:56:55 -0500

    Lifestyle methods

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,24 +2,26 @@ import React, {Component} from 'react'
 import Title from './Title'
 import PhotoWall from './PhotoWall'
 
+// const posts = [{
+//     id: "0",
+//     description: "beautiful landscape",
+//     imageLink: "https://i1.sndcdn.com/artworks-000233950889-o4qwrx-t500x500.jpg"
+//     }, {
+//     id: "1",
+//     description: "Aliens???",
+//     imageLink: "https://i1.sndcdn.com/artworks-y8PtAmwRG5GxWZTq-utgsiA-t500x500.jpg"
+//     }, {
+//     id: "2",
+//     description: "On a vacation!",
+//     imageLink: "https://i1.sndcdn.com/artworks-hsj0ylvz2BQEo78P-ui2Uzw-t500x500.jpg"
+// }];
+
 class Main extends Component{
 
     constructor(){
         super();
         this.state = {
-            posts: [{
-                id: "0",
-                description: "beautiful landscape",
-                imageLink: "https://i1.sndcdn.com/artworks-000233950889-o4qwrx-t500x500.jpg"
-                }, {
-                id: "1",
-                description: "Aliens???",
-                imageLink: "https://i1.sndcdn.com/artworks-y8PtAmwRG5GxWZTq-utgsiA-t500x500.jpg"
-                }, {
-                id: "2",
-                description: "On a vacation!",
-                imageLink: "https://i1.sndcdn.com/artworks-hsj0ylvz2BQEo78P-ui2Uzw-t500x500.jpg"
-            }]
+            posts: []
         }
         this.removePhoto = this.removePhoto.bind(this);
     }
@@ -27,6 +29,11 @@ class Main extends Component{
     removePhoto(postRemoved){
         console.log(postRemoved.description);
         this.setState((state) => ({posts: state.posts.filter((post) => post !== postRemoved)}))
+    }
+
+    componentDidMount(){
+        const data = SimulateFetchFromDatabase()
+        this.setState({posts: data})
     }
 
     render(){
@@ -37,4 +44,20 @@ class Main extends Component{
     }
 }
 
-  export default Main
+function SimulateFetchFromDatabase(){
+    return [{
+            id: "0",
+            description: "beautiful landscape",
+            imageLink: "https://i1.sndcdn.com/artworks-000233950889-o4qwrx-t500x500.jpg"
+            }, {
+            id: "1",
+            description: "Aliens???",
+            imageLink: "https://i1.sndcdn.com/artworks-y8PtAmwRG5GxWZTq-utgsiA-t500x500.jpg"
+            }, {
+            id: "2",
+            description: "On a vacation!",
+            imageLink: "https://i1.sndcdn.com/artworks-hsj0ylvz2BQEo78P-ui2Uzw-t500x500.jpg"
+        }]
+}
+
+export default Main
