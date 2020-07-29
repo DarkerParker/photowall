63be0cdcb2e7be20aefe4f269909d76d72eb2c04
Parker Johnson <plj06370@creighton.edu>
2020-07-29 11:18:50 -0500

    Updating state

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,28 +2,37 @@ import React, {Component} from 'react'
 import Title from './Title'
 import PhotoWall from './PhotoWall'
 
-const posts = [{
-    id: "0",
-    description: "beautiful landscape",
-    imageLink: "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg" +
-    "3919321_1443393332_n.jpg"
-    }, {
-    id: "1",
-    description: "Aliens???",
-    imageLink: "https://img.buzzfeed.com/buzzfeed-static/static/2015-10/27/17/enhanced/webdr11/enhanced-mid-29607-1445980104-1.jpg?downsize=700%3A%2A&output-quality=auto&output-format=auto" +
-    "08323785_735653395_n.jpg"
-    }, {
-    id: "2",
-    description: "On a vacation!",
-    imageLink: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg"
-    }
-]
-
 class Main extends Component{
+
+    constructor(){
+        super();
+        this.state = {
+            posts: [{
+                id: "0",
+                description: "beautiful landscape",
+                imageLink: "https://i1.sndcdn.com/artworks-000233950889-o4qwrx-t500x500.jpg"
+                }, {
+                id: "1",
+                description: "Aliens???",
+                imageLink: "https://i1.sndcdn.com/artworks-y8PtAmwRG5GxWZTq-utgsiA-t500x500.jpg"
+                }, {
+                id: "2",
+                description: "On a vacation!",
+                imageLink: "https://i1.sndcdn.com/artworks-hsj0ylvz2BQEo78P-ui2Uzw-t500x500.jpg"
+            }]
+        }
+        this.removePhoto = this.removePhoto.bind(this);
+    }
+
+    removePhoto(postRemoved){
+        console.log(postRemoved.description);
+        this.setState((state) => ({posts: state.posts.filter((post) => post !== postRemoved)}))
+    }
+
     render(){
         return (<div>
                     <Title title = {'Photowall'}/>
-                    <PhotoWall posts = {posts}/>
+                    <PhotoWall posts = {this.state.posts} onRemovePhoto = {this.removePhoto}/>
                 </div>)
     }
 }
